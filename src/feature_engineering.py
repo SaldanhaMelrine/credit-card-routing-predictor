6894bd176ea8af172550aca0f7dbc63c0f229102
SaldanhaMelrine <60963782+SaldanhaMelrine@users.noreply.github.com>
2025-01-28 20:10:55 +0100

    Update feature_engineering.py

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,11 +1,13 @@
+# Features and Target
 def define_features_and_target(df):
 
     output_path= r"C:/Users/dell/Desktop/M.Sc. Data Science/Case Study Model Engineering/credit-card-routing-predictor/data/processed/processed_data.xlsx"
 
     features = [
-        'amount', 'cumulative_fee', 'attempt_number',
-        'psp_success_rate', 'psp_country_success_rate', 'psp_success_cost', 'psp_failure_cost'
-    ] + [col for col in df.columns if col.startswith('country_') or col.startswith('card_') or col.startswith('3D_secured_')]
+        'amount', 'avg_psp_reattempts', 'avg_cumulative_fee', 'cumulative_fee', 'attempt_number',
+        'psp_success_rate', 'psp_country_success_rate', 'psp_success_cost', 'psp_failure_cost',
+        'PSP','country', 'card','3D_secured'
+    ] 
 
     target = 'success'
 
@@ -13,3 +15,4 @@ def define_features_and_target(df):
     print(f"Processed data saved to: {output_path}")
 
     return df[features], df[target]
+
